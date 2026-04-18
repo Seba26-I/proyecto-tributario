6b9644f3af1ace84f520875e826b2f3576ab0a01
Sebastian <sebastian.monsalve10@inacapmail.cl>
2026-04-18 00:47:33 -0400

    prueba de formulario y validaciones

--- src/App.js
+++ src/App.js
@@ -14,11 +14,37 @@ function App() {
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(form);
-    alert("Datos enviados");
-  };
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  // Validación
+  if (!form.anio || !form.monto || !form.factor) {
+    alert("Todos los campos son obligatorios");
+    return;
+  }
+
+  try {
+    const res = await fetch("http://localhost:3000/registros", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        anio: Number(form.anio),
+        monto: Number(form.monto),
+        factor: Number(form.factor)
+      })
+    });
+
+    const data = await res.json();
+
+    alert("Respuesta del servidor: " + data.mensaje);
+
+  } catch (error) {
+    console.error(error);
+    alert("Error conectando con el backend");
+  }
+};
 
   return (
     <div style={{ padding: "20px" }}>
